f8ce14e6a56576f62b27cd6c80d02bc90e35f3bd
oriol <oriol@DESKTOP-FAN64NF>
2022-10-12 16:49:27 +0200

    Classe Dades

--- src/main/java/server/data/Dades.java
+++ src/main/java/server/data/Dades.java
@@ -13,10 +13,17 @@ import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.InfluxDBClientFactory;
 import com.influxdb.client.QueryApi;
 import com.influxdb.client.WriteApiBlocking;
+import com.influxdb.client.domain.Authorization;
+import com.influxdb.client.domain.Bucket;
+import com.influxdb.client.domain.BucketRetentionRules;
+import com.influxdb.client.domain.Organization;
+import com.influxdb.client.domain.Permission;
+import com.influxdb.client.domain.PermissionResource;
 import com.influxdb.client.domain.WritePrecision;
 import com.influxdb.client.write.Point;
 import com.influxdb.query.FluxRecord;
 import com.influxdb.query.FluxTable;
+import java.util.Arrays;
 import server.machine.Menjadora;
 
 /**
@@ -53,6 +60,59 @@ public class Dades {
     }
     
     //FUNCIONS
+    
+    public void creaOrg(){
+                InfluxDBClient influxDBClient = InfluxDBClientFactory.create("http://localhost:8086", token);
+
+        Organization org = influxDBClient.getOrganizationsApi()
+                .createOrganization("Menjadora 1" + System.currentTimeMillis());
+
+        String menjadoraId =  org.getId();
+        creaBuckets(org);
+        
+ 
+    }
+    
+    public void creaBuckets(Organization org){ 
+        InfluxDBClient influxDBClient = InfluxDBClientFactory.create("http://localhost:8086", token);
+
+        //
+        // Create bucket "iot_bucket" with data retention set to 3,600 seconds
+        //
+        BucketRetentionRules retention = new BucketRetentionRules();
+        retention.setEverySeconds(3600);
+
+        Bucket bucket = influxDBClient.getBucketsApi().createBucket("iot-bucket", retention, "12bdc4164c2e8141");
+
+        //
+        // Create access token to "iot_bucket"
+        //
+        PermissionResource resource = new PermissionResource();
+        resource.setId(bucket.getId());
+        resource.setOrgID("12bdc4164c2e8141");
+        resource.setType(PermissionResource.TYPE_BUCKETS);
+
+        // Read permission
+        Permission read = new Permission();
+        read.setResource(resource);
+        read.setAction(Permission.ActionEnum.READ);
+
+        // Write permission
+        Permission write = new Permission();
+        write.setResource(resource);
+        write.setAction(Permission.ActionEnum.WRITE);
+
+        Authorization authorization = influxDBClient.getAuthorizationsApi()
+                .createAuthorization("12bdc4164c2e8141", Arrays.asList(read, write));
+
+        //
+        // Created token that can be use for writes to "iot_bucket"
+        //
+        String token = authorization.getToken();
+        System.out.println("Token: " + token);
+
+        influxDBClient.close();
+    }
     public void escriuDades(double gramsAcumulatAvui){
         
         InfluxDBClient influxDBClient = InfluxDBClientFactory.create("http://localhost:8086", token, org, bucket);
