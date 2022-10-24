51ee8279f49425647aa2f955defba9ce7186af05
MrOscarvs <oscarvilluendas7@gmail.com>
2022-10-24 19:52:01 +0200

    Login amb tokens

--- src/main/java/login/Server/Server.java
+++ src/main/java/login/Server/Server.java
@@ -1,11 +1,15 @@
 package login.Server;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import login.Constants;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketAddress;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Server {
 
@@ -13,27 +17,132 @@ public class Server {
     static String host = "localhost";
     static int port = 9999;
     protected Socket socket;
+    Map<String, String> loggedClients = new HashMap<String, String>();
+    Map<String, Long> caducitatClients = new HashMap<String, Long>(); //
+    Map<String, String> usuaris = Map.of(     // Provisional, s'haurà de llegir d'una base de dades.
+            "usuari1", "contrassenya1",
+            "usuari2", "contrassenya2",
+            "usuari3", "contrassenya3"
+    );
 
-    public void setSocket(Socket socket) {
-        this.socket = socket;
+
+    private static final SecureRandom secureRandom = new SecureRandom(); //threadsafe
+    private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder(); //threadsafe
+
+    public static void main(String[] args) throws Exception {
+
+        Server servidor = new Server();
+
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("El servidor s'està executant...");
+        while (true) {
+            System.out.println("Esperant connexions ...");
+            servidor.setSocket(serverSocket.accept());
+            System.out.println("Client connectat");
+            servidor.executa();
+        }
+        //serverSocket.close();
+
+
     }
 
+
     public void executa() {
+        String comanda;
+        BufferedReader lector;
+        try {
+            lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            comanda = lector.readLine();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        switch (comanda) {
+            case "login":
+                System.out.println("Executant comanda de login");
+                loginClient(lector);
+                break;
+            case "check":
+                System.out.println("Executant comanda de check");
+                try {
+                    String userToken = lector.readLine();
+                    comprovaCaducitat(userToken);
+                    if (loggedClients.containsKey(userToken)) {
+                        sendSocketOut(loggedClients.get(userToken));
+                    } else {
+                        sendSocketOut("Usuari no trobat");
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case "logout":
+                System.out.println("Executant comanda de logout");
+                try {
+                    String userToken = lector.readLine();
+                    comprovaCaducitat(userToken);
+                    if (loggedClients.containsKey(userToken)) {
+                        loggedClients.remove(userToken);
+                        sendSocketOut("true");
+                    } else {
+                        sendSocketOut("false");
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+        }
+    }
+
+    private void comprovaCaducitat(String token) {
+        if (caducitatClients.containsKey(token)) {
+            if (System.currentTimeMillis() - caducitatClients.get(token) >= Constants.CADUCITAT_CLIENTS) {
+                loggedClients.remove(token);
+                caducitatClients.remove(token);
+            }
+        }
+    }
+    private void loginClient(BufferedReader lector) {
         String usuari = "";
         String contrassenya = "";
         try {
-            BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             usuari = lector.readLine();
             contrassenya = lector.readLine();
         } catch (Exception e) {
             e.printStackTrace();
+            return;
         }
-        boolean resultat = checkCredentials(usuari, contrassenya);
+        if (checkCredentials(usuari, contrassenya)) {
+            String loginToken = generateNewToken();
+            loggedClients.put(loginToken, usuari);
+            caducitatClients.put(loginToken, System.currentTimeMillis());
+            sendSocketOut("true", loginToken);
+        } else {
+            sendSocketOut("false");
+        }
+    }
+
+    private boolean checkCredentials(String usuari, String contrassenya) {
+        if (usuaris.containsKey(usuari)) {
+            if (contrassenya.equals(usuaris.get(usuari))) {
+                System.out.println("Credencials correctes per l'usuari: " + usuari);
+                return true;
+            }
+        }
+        System.out.println("Credencials INcorrectes per l'usuari: " + usuari);
+        return false;
+    }
+
+    private void sendSocketOut(String... args) {
         try {
             BufferedWriter escriptor = new BufferedWriter(
                     new OutputStreamWriter(socket.getOutputStream()));
-            escriptor.write(String.valueOf(resultat));
+            escriptor.write(String.valueOf(args.length));
             escriptor.newLine();
+            for (String arg : args) {
+                escriptor.write(arg);
+                escriptor.newLine();
+            }
             escriptor.flush();
             socket.close();
         } catch (Exception e) {
@@ -41,35 +150,14 @@ public class Server {
         }
     }
 
-    //El protocol per a la operació és operador:valor1:valor2
-    private boolean checkCredentials (String usuari, String contrassenya) {
-        //System.out.println("Usuari -" + usuari + "-");
-        //System.out.println("Contrassenya -" + contrassenya + "-");
-        if (usuari.equals("user") && contrassenya.equals("password") ){
-            System.out.println("Credencials correctes");
-            return true;
-        } else {
-            System.out.println("Credencials INcorrectes");
-            return false;
-        }
+    public static String generateNewToken() {
+        byte[] randomBytes = new byte[24];
+        secureRandom.nextBytes(randomBytes);
+        return base64Encoder.encodeToString(randomBytes);
     }
 
 
-    public static void main(String[] args) throws Exception {
-
-        Server servidor = new Server();
-
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("El servidor de credencials s'esta executant...");
-        while (true) {
-            System.out.println("Esperant clients ...");
-            servidor.setSocket(serverSocket.accept());
-            System.out.println("Client connectat");
-            servidor.executa();
-        }
-        /*serverSocket.close();
-        System.out.println("El servidor matemàtic s'ha tancat...");*/
-
-
+    public void setSocket(Socket socket) {
+        this.socket = socket;
     }
 }
